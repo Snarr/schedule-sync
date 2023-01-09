dc73f5f867c291da03450d8058f12b0cb1e2e990
Jacob Snarr <jacobsnarr@gmail.com>
2023-01-09 14:38:45 -0500

    Add experimental flat UI and Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,20 @@
+import { MouseEventHandler } from "react";
+
+function Button({
+  children,
+  onClick
+  }: {
+    children: string,
+    onClick: MouseEventHandler<HTMLDivElement> | undefined;
+  }) {
+
+  return (
+    <>
+      <div key="calendar-event" className="w-full h-fit px-6 py-4 bg-white rounded-full flex flex-row justify-center items-center cursor-pointer select-none" onClick={onClick}>
+        {children}
+      </div>
+    </>
+  )
+}
+
+export default Button
--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -13,10 +13,13 @@ function EventCard({
 
   return (
     <>
-      <div key="calendar-event" className="w-full h-fit px-6 py-4 rounded-full bg-white flex flex-row gap-3 items-center shadow-3xl">
-        <Twemoji options={{ className: 'twemoji' }}>
+      <div key="calendar-event" className="w-full h-fit px-6 py-4 bg-white flex flex-row gap-3 items-center border-b-2 select-none">
+        {/* <Twemoji options={{ className: 'twemoji' }}>
           {icon}
-        </Twemoji>
+        </Twemoji> */}
+        <div>
+          {icon}
+        </div>
         <div key="details" className="flex flex-col items-start">
           <div key="times" className="flex flex-row justify-start items-center gap-2">
             <div key="name" className="font-bold text-xl">
