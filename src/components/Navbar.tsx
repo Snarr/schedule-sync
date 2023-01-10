5bee161fe2a223ef82681c5df8ee5a0906e4f491
Jacob Snarr <jacobsnarr@gmail.com>
2023-01-10 01:40:31 -0500

    Refactor Navbar and App structure

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,19 @@
+import Link from "next/link";
 import Button from "./Button";
+
+const pages = [
+  "Schedule",
+  "Friends",
+  "Account"
+]
 
 function Navbar() {
   return (<div className="w-full px-2 py-1 flex flex-row justify-center items-center gap-2">
-            <Button>
-              Events
-            </Button>
-            <Button>
-              Friends
-            </Button>
-            <Button>
-              Account
-            </Button>
+            {pages.map((page) => (
+              <Link key={page} href={`/${page.toLowerCase()}`} className="w-full">
+                <Button>{page}</Button>
+              </Link>
+            ))}
           </div>)
 }
 
